393f7ef767b445756391bc24f0e4ef0525a3dcf1
IaraDeSousa <xxiarasousaxx@live.co.uk>
2026-03-16 17:18:36 +0000

    submission

--- Level 1 - Python for Software Engineering/M02T07 – Programming with Built-in Functions/jokes.py
+++
@@ -1,8 +0,0 @@
-import random as ran
-
-jokes_list = ["Is your dad a baker? Because you've got some great buns",
-              "Put the punchline in the title. How do you ruin a joke?",
-              "Two fish are swimming in a lake and one runs into a concrete wall. It turns to the other and says, 'Dam'"]
-
-# Prints one of the three jokes at random
-print(ran.choice(jokes_list))
--- Level 2 - Introduction to Software Engineering/M06T04 – Django – Sticky Notes Application Part 1/sticky_notes/notes/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class NotesConfig(AppConfig):
-    """
-    Configuration class for the notes application.
-    """
-    name = 'notes'
